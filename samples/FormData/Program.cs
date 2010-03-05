04d5b32f3fd7e3ca2df5e669b96fb977b395f559
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-05 20:50:28 +0000

    - Added a FormData sample

---
+++ samples/FormData/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Fu;
+using Fu.Presets;
+using Fu.Services.Web;
+using Fu.Steps;
+
+namespace FormData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new SimpleApp(fu.Map.Urls(
+                new UrlMap("^/$", fu.Static.File("index.html")),
+                new UrlMap("^/save$", c =>
+                {
+                    var forms = c.Get<IFormData>();
+                    var name = forms["name"];
+
+                    return fu.Static.Text(name)(c);
+                })
+            ));
+
+            app.Services.Add(new FormDataParser());
+            app.Start();
+        }
+    }
+}
