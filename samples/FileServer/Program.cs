2b642768f7a80787814f0373bb12a5d4e4ec0a2f
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-05 21:52:17 +0000

    - Created a File server sample

---
+++ samples/FileServer/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.IO;
+using System.Linq;
+
+using Fu;
+using Fu.Presets;
+using Fu.Results;
+using Fu.Steps;
+
+namespace FileServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var step = fu.Map.DefaultDoc(listFiles, fu.Static.Folder("/", "Content"));
+
+            var app = new SimpleApp(step);
+            app.Start();
+        }
+
+        static IFuContext listFiles(IFuContext c)
+        {
+            var folder = c.ResolvePath("Content");
+            var files = Directory
+                .GetFiles(folder, "*.*", SearchOption.AllDirectories)
+                .Select(f => f.Substring(folder.Length))
+                .Select(f => string.Format("<li><a href=\"{0}\">{0}</a></li>", f))
+                .ToArray();
+
+            var template = @"
+                <html>
+                    <head><title>Files on this server</title></head>
+                    <body><h1>Files</h1><ul>{0}</ul></body>
+                </html>";
+
+            var html = string.Format(template, string.Join("", files));
+
+            var result = StringResult.From(c, html);
+            result.Result.ContentType.MediaType = Mime.TextHtml;
+
+            return result;
+        }
+    }
+}
