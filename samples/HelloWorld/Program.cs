6e03fd0bf4df6cf18a60d8e9414243a33c4716d5
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-05 20:42:22 +0000

    - Added a basic Hello World app

---
+++ samples/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Fu;
+using Fu.Presets;
+using Fu.Steps;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new SimpleApp(fu.Static.Text("Hello World!!"));
+            app.Start();
+        }
+    }
+}
