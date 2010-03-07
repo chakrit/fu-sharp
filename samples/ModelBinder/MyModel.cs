db193f401e722a13400eba95447cf7da86d8e7d1
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-07 18:32:51 +0000

    - Implemented a ModelBinder sample

---
+++ samples/ModelBinder/MyModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ModelBinder
+{
+    public class MyModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ samples/ModelBinder/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Fu;
+using Fu.Presets;
+using Fu.Services.Models;
+using Fu.Services.Web;
+using Fu.Steps;
+
+namespace ModelBinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new SimpleApp(fu.Map.Urls(
+                new UrlMap("^/$", fu.Static.File("index.html")),
+                new UrlMap("^/greet$", fu.Http.Post(c =>
+                {
+                    var model = c.Get<MyModel>();
+                    var greets = string.Format(
+                        "Hello, {0}! You're {1} years old!",
+                        model.Name, model.Age);
+
+                    c.WalkPath.InsertNext(fu.Static.Text(greets));
+                    return c;
+                }))));
+
+            app.Services.Add(new ModelBinder<MyModel>());
+            app.Services.Add(new FormDataParser());
+            app.Start();
+        }
+    }
+}
