8cb6170a7252992db14fa66979b06c1c69ba3d22
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-09 11:17:08 +0000

    - Added an ObjectProvider for providing simple static objects.

---
+++ src/Fu/Services/ObjectProvider.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Fu.Services
+{
+    // simple service to provide a static singleton object
+    // such as settings throughout the app
+    public class ObjectProvider<T> : IService<T>
+    {
+        private T _obj;
+
+        public ObjectProvider(T obj) { _obj = obj; }
+
+
+        public bool CanGetServiceObject(IFuContext input) { return _obj != null; }
+        public T GetServiceObject(IFuContext input) { return _obj; }
+
+        public void BeginWalk(IFuContext input) { /* no-op */ }
+        public void EndWalk(IFuContext input) { /* no-op */ }
+    }
+}
