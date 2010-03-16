9378c9d9cce503c24dd099501ab8c4df630a0573
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-16 14:42:11 +0000

    - Add a PAUSE before pushing so the github link doesn't timeout if I didn't pay attention

--- sync_github.bat
+++ sync_github.bat
@@ -5,6 +5,8 @@ IF EXIST git_sync_temp RD /s git_sync_temp
 CMD /C git svn clone -s https://buildsvnwiki.myidxnet.localdomain/svn/FuSharp git_sync_temp --no-metadata
 CD git_sync_temp
 CMD /C git remote add origin git@github.com:chakrit/fu-sharp.git
+
+PAUSE
 CMD /C git push origin master
 
 CD ..
