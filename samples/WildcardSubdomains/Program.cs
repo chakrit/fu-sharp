21573d727d62cc1711b23012bfac1e263bb41a3e
chakrit <chakrit@bb9a86ef-2f06-3442-baea-32778e8f79ca>
2010-03-25 06:26:48 +0000

    - Added a WildcardSubdomain sample

---
+++ samples/WildcardSubdomains/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.IO;
+
+using Fu;
+
+namespace WildcardSubdomains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null, null, new Step[] { EchoSubdomain });
+
+            // accept all port 80 connection, regardless of domain
+            app.Settings.Host = "*";
+            app.Start();
+        }
+
+        static IFuContext EchoSubdomain(IFuContext c)
+        {
+            var msg = c.Request.Headers["host"];
+            msg = "You are browsing from: " + msg;
+
+            var sw = new StreamWriter(c.Response.OutputStream);
+            sw.Write(msg);
+            sw.Flush();
+
+            return c;
+        }
+    }
+}
---
+++ samples/WildcardSubdomains/readme.txt
@@ -0,0 +1,14 @@
+﻿
+# You can supply a FuSettings instance with `settings.Host = "*"` to make
+# Fu# recognize all hosts (it defaults to localhost for security reasons)
+#
+# To enable wildcard subdomain, however, will also require that you have
+# proper DNS lookup setup as well which can be worked around locally
+# by pasting the content of this file to your machine's hosts file
+# typically at C:\Windows\System32\drivers\etc\hosts
+#
+# Then start the project and try http://x.localhost/ and http://y.localhost/
+
+127.0.0.1    localhost
+127.0.0.1    x.localhost
+127.0.0.1    y.localhost
