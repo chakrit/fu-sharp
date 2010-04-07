22f1324e254b1debdd639041a813738a47c5c9a7
Chakrit Wichian <chakrit@2nitedesign.com>
2010-04-07 17:00:48 +0700

    Implemented a ServiceBroker to separate service resolving code from the context

---
+++ src/Fu/Delegates.cs
@@ -0,0 +1,9 @@
+﻿
+using System.Net;
+
+namespace Fu
+{
+  public delegate void FuAction(IFuContext c);
+
+  public delegate FuAction Continuation(FuAction action);
+}
---
+++ src/Fu/Services/IServiceBroker.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+namespace Fu.Services
+{
+  public interface IServiceBroker
+  {
+    IEnumerable<IService> Services { get; }
+
+    T Get<T>(IFuContext c);
+    bool CanGet<T>(IFuContext c);
+  }
+}
