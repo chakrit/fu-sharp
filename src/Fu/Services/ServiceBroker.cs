269497a3a1198107d9d4cd35575fb42022979424
Chakrit Wichian <chakrit@2nitedesign.com>
2010-04-10 21:19:08 +0700

    Fixed a duplicate service problem in ServiceBroker

--- src/Fu/Services/ServiceBroker.cs
+++ src/Fu/Services/ServiceBroker.cs
@@ -9,22 +9,17 @@ namespace Fu.Services
 {
   public class ServiceBroker : IServiceBroker
   {
-    private IDictionary<Type, IService> _serviceMap;
-
-
     public IEnumerable<IService> Services { get; private set; }
 
     public ServiceBroker(IEnumerable<IService> services)
     {
       Services = services;
-
-      _serviceMap = new Dictionary<Type, IService>();
     }
 
 
     public T Get<T>(IFuContext c)
     {
-      var service = getService<T>();
+      var service = getService<T>(c);
       if (service == null)
         throw new InvalidServiceTypeException(typeof(T));
 
@@ -33,7 +28,7 @@ namespace Fu.Services
 
     public bool CanGet<T>(IFuContext c)
     {
-      var service = getService<T>();
+      var service = getService<T>(c);
 
       return service != null &&
         service.CanGetServiceObject(c);
@@ -42,19 +37,11 @@ namespace Fu.Services
 
     // TODO: There's seem to be some GetHashCode optimization possible
     //       mentioned in Funq IoC source code... maybe we can use that?
-    private IService<T> getService<T>()
+    private IService<T> getService<T>(IFuContext c)
     {
-      var type = typeof(T);
-
-      if (_serviceMap.ContainsKey(typeof(T)))
-        return (IService<T>)_serviceMap[type];
-
-      var service = Services
+      return Services
         .OfType<IService<T>>()
-        .FirstOrDefault();
-
-      _serviceMap[type] = service;
-      return service;
+        .FirstOrDefault(srv => srv.CanGetServiceObject(c));
     }
   }
 }
