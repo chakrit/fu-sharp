4345d12088aca09e41da3fca40610368dc150ab4
Chakrit Wichian <chakrit@2nitedesign.com>
2010-04-10 21:58:23 +0700

    FuContext.As<T>() now checks any previous contextes for compatible contexts to increase ease of use.

--- src/Fu/FuContext.cs
+++ src/Fu/FuContext.cs
@@ -11,6 +11,9 @@ namespace Fu
 {
   public class FuContext : IFuContext, IDisposable
   {
+    private IFuContext _previous;
+
+
     // TODO: Abstract out HttpListenerRequest
     public HttpListenerRequest Request { get; private set; }
     public HttpListenerResponse Response { get; private set; }
@@ -18,33 +21,53 @@ namespace Fu
     public IServiceBroker Services { get; private set; }
     public FuSettings Settings { get; private set; }
 
+
     public FuContext(IFuContext c) :
-      this(c.Settings, c.Services, c.Request, c.Response) { }
+      this(c, c.Settings, c.Services, c.Request, c.Response) { }
 
     public FuContext(FuSettings settings,
       IServiceBroker services,
       HttpListenerContext httpContext) :
-      this(settings, services, httpContext.Request, httpContext.Response) { }
+      this(null, settings, services, httpContext.Request, httpContext.Response) { }
 
     public FuContext(FuSettings settings,
       IServiceBroker services,
+      HttpListenerRequest request,
+      HttpListenerResponse response) :
+      this(null, settings, services, request, response) { }
+
+    protected FuContext(IFuContext previous,
+      FuSettings settings,
+      IServiceBroker broker,
       HttpListenerRequest request,
       HttpListenerResponse response)
     {
       this.Request = request;
       this.Response = response;
 
-      this.Services = services;
+      this.Services = broker;
       this.Settings = settings;
+
+      _previous = previous;
     }
 
 
     public T As<T>() where T : class, IFuContext
     {
+      // TODO: Is looking up the chain a good idea?
+      //       since this will transparently transforms the context
+      //       without the user acknowledgement
       var result = this as T;
-      if (result == null)
-        throw new MismatchedContextTypeException(
-          this.GetType(), typeof(T));
+      if (result == null) {
+        if (_previous == null)
+          throw new MismatchedContextTypeException(
+            this.GetType(), typeof(T));
+
+        result = _previous.As<T>();
+        if (result == null)
+          throw new MismatchedContextTypeException(
+            this.GetType(), typeof(T));
+      }
 
       return result;
     }
