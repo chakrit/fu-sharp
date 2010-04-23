758f973ed582bf4b657bb8ec36f6bb101441bd06
Chakrit Wichian <chakrit@2nitedesign.com>
2010-04-24 02:28:57 +0700

    Added Static.Json method

---
+++ src/Fu.Web/Steps/StaticEx.cs
@@ -0,0 +1,18 @@
+﻿
+using Fu.Results;
+
+namespace Fu.Steps
+{
+  public static class StaticEx
+  {
+    public static Continuation Json(this IStaticSteps _, object obj)
+    {
+      return _.Json(ctx => obj);
+    }
+
+    public static Continuation Json(this IStaticSteps _, Reduce<object> objReducer)
+    {
+      return step => ctx => step(JsonResult.From(ctx, objReducer(ctx)));
+    }
+  }
+}
