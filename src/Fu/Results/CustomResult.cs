19960218e9c012f073eba64237f4b95566e33f95
Chakrit Wichian <buhtig@chakrit.net>
2010-07-06 16:39:27 +0700

    Added a CustomResult to allow customized rendering.

---
+++ src/Fu/Results/CustomResult.cs
@@ -0,0 +1,24 @@
+﻿
+using System.IO;
+
+namespace Fu.Results
+{
+  public delegate long RenderDelegate(IFuContext c, Stream output);
+
+  public class CustomResult : ResultBase
+  {
+    public RenderDelegate _render;
+
+    public CustomResult(RenderDelegate renderAction)
+    {
+      // TODO: Null-check
+      _render = renderAction;
+    }
+
+
+    public override long Render(IFuContext c, Stream output)
+    {
+      return _render(c, output);
+    }
+  }
+}
