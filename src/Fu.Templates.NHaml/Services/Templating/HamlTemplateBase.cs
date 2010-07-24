91a9ec009e0abba7be8682654ec1f8a29333228e
Chakrit Wichian <chakrit@2nitedesign.com>
2010-07-24 15:41:26 +0700

    Add basic template blocks support.

--- src/Fu.Templates.NHaml/Services/Templating/HamlTemplateBase.cs
+++ src/Fu.Templates.NHaml/Services/Templating/HamlTemplateBase.cs
@@ -1,13 +1,42 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 using Fu.Results;
 
 using NHaml;
 
 namespace Fu.Services.Templating
 {
-    public class HamlTemplateBase : Template
+  public class HamlTemplateBase : Template
+  {
+    private IDictionary<string, Stack<Action>> _blocks;
+
+    public HamlResultBase Result { get; set; }
+    public IFuContext Context { get; set; }
+
+
+    public void Block(string name, Action yield)
     {
-        public HamlResultBase Result { get; set; }
-        public IFuContext Context { get; set; }
+      _blocks = _blocks ?? new Dictionary<string, Stack<Action>>(5);
+
+      Stack<Action> stack;
+
+      if (!_blocks.TryGetValue(name, out stack))
+        _blocks[name] = stack = new Stack<Action>(5);
     }
+
+    public void RenderBlock(string name)
+    {
+      if (_blocks == null) return;
+
+      Stack<Action> stack;
+
+      if (!_blocks.TryGetValue(name, out stack))
+        return;
+
+      if (stack.Count > 0)
+        stack.Pop()();
+    }
+  }
 }
